8b71127b57b88b9124b8a30600f03dd7e782e1fa
illocarlos <carlosalbendiz93@gmail.com>
2024-02-28 12:55:39 +0100

    loader

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
     return (
 
-        <div className="animate-pulse flex flex-col items-center gap-4 w-60">
+        <div className="animate-pulse flex flex-col justify-center items-center gap-4 w-60">
             <div>
                 <div className="w-48 h-6 bg-slate-400 rounded-md"></div>
                 <div className="w-28 h-4 bg-slate-400 mx-auto mt-3 rounded-md"></div>
