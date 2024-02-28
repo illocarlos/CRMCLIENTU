3d7255c96abe04eaf8283bbcdcbef126f6fb2be2
illocarlos <carlosalbendiz93@gmail.com>
2024-02-28 13:11:21 +0100

    ok

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import './Loader.css'
+
 
 const Loader = () => {
     return (
 
-        <div className="loader">
-            <span className="loader-text">loading</span>
-            <span className="load"></span>
+        <div className="flex flex-row gap-2">
+            <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
+            <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
+            <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
         </div>
-
 
     )
 }
